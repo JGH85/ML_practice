fd58bece83155432aa8235800a3a3173285f270e
Joseph Houser <josephghouser@gmail.com>
2020-02-04 23:22:33 -0600

    added some notes on simple linear regression

--- Machine Learning/Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.py
+++ Machine Learning/Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.py
@@ -14,7 +14,7 @@ y = dataset.iloc[:, 1].values
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
 
-# Feature Scaling
+# Feature Scaling (not needed for linear regression since library takes care of it)
 """from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
@@ -32,6 +32,7 @@ y_pred = regressor.predict(X_test)
 
 # Visualising the Training set results
 plt.scatter(X_train, y_train, color = 'red')
+#create the prediction line using X_train and predicted values
 plt.plot(X_train, regressor.predict(X_train), color = 'blue')
 plt.title('Salary vs Experience (Training set)')
 plt.xlabel('Years of Experience')
@@ -42,6 +43,15 @@ plt.show()
 plt.scatter(X_test, y_test, color = 'red')
 plt.plot(X_train, regressor.predict(X_train), color = 'blue')
 plt.title('Salary vs Experience (Test set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+#visualize test and train together
+plt.scatter(X_train, y_train, color = 'black')
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience: Training set (black) and Test set (red)')
 plt.xlabel('Years of Experience')
 plt.ylabel('Salary')
 plt.show()
