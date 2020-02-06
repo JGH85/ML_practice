ec15f6ba2b77233f8bd02ce637ad5710d7194c1e
Joseph Houser <josephghouser@gmail.com>
2020-02-05 22:47:22 -0600

    worked through multiple linear regression examples

--- Machine Learning/Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
+++ Machine Learning/Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
@@ -19,6 +19,7 @@ X = onehotencoder.fit_transform(X).toarray()
 
 # Avoiding the Dummy Variable Trap
 X = X[:, 1:]
+#technically we don't need to do this because the library will take care of it but it doesn't hurt to
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
@@ -39,3 +40,92 @@ regressor.fit(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = regressor.predict(X_test)
+
+#use backward elimination to build optimal model
+#backward elimination steps:
+# 1. choose significance level (SL) to stay in
+# 2. fit full model with all predictors
+# 3. consider highest P value. if P > SL, go to 4. 
+# 4. remove predictor. 
+# 5. refit model and return to 3. 
+
+import statsmodels.formula.api as sm
+#we need to add x0 = 1 to account for b0 in multilinear regression formula because statsmodel library doesn't automatically add it
+X = np.append(arr = np.ones((50,1)).astype(int), values = X, axis = 1)
+
+#create optimal matrix of features to hold the features that are significant
+#we select all to start
+X_opt = X[:,[0,1,2,3,4,5]]
+
+#OLS stands for ordinary least squares
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+#remove highest and repeat
+X_opt = X[:,[0,1,3,4,5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+#remove highest and repeat
+X_opt = X[:,[0,3,4,5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+
+#remove highest and repeat
+X_opt = X[:,[0,3,5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+#remove highest and repeat
+X_opt = X[:,[0,3]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+
+#import statsmodels.formula.api as sm
+#def backwardElimination(x, sl):
+#    numVars = len(x[0])
+#    for i in range(0, numVars):
+#        regressor_OLS = sm.OLS(y, x).fit()
+#        maxVar = max(regressor_OLS.pvalues).astype(float)
+#        if maxVar > sl:
+#            for j in range(0, numVars - i):
+#                if (regressor_OLS.pvalues[j].astype(float) == maxVar):
+#                    x = np.delete(x, j, 1)
+#    regressor_OLS.summary()
+#    return x
+# 
+#SL = 0.05
+#X_opt = X[:, [0, 1, 2, 3, 4, 5]]
+#X_Modeled = backwardElimination(X_opt, SL)
+#Backward Elimination with p-values and Adjusted R Squared:
+#
+#import statsmodels.formula.api as sm
+#def backwardElimination(x, SL):
+#    numVars = len(x[0])
+#    temp = np.zeros((50,6)).astype(int)
+#    for i in range(0, numVars):
+#        regressor_OLS = sm.OLS(y, x).fit()
+#        maxVar = max(regressor_OLS.pvalues).astype(float)
+#        adjR_before = regressor_OLS.rsquared_adj.astype(float)
+#        if maxVar > SL:
+#            for j in range(0, numVars - i):
+#                if (regressor_OLS.pvalues[j].astype(float) == maxVar):
+#                    temp[:,j] = x[:, j]
+#                    x = np.delete(x, j, 1)
+#                    tmp_regressor = sm.OLS(y, x).fit()
+#                    adjR_after = tmp_regressor.rsquared_adj.astype(float)
+#                    if (adjR_before >= adjR_after):
+#                        x_rollback = np.hstack((x, temp[:,[0,j]]))
+#                        x_rollback = np.delete(x_rollback, j, 1)
+#                        print (regressor_OLS.summary())
+#                        return x_rollback
+#                    else:
+#                        continue
+#    regressor_OLS.summary()
+#    return x
+# 
+#SL = 0.05
+#X_opt = X[:, [0, 1, 2, 3, 4, 5]]
+#X_Modeled = backwardElimination(X_opt, SL)
